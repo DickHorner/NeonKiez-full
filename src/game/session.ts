e9818cdf730b6928ecd92b87cf899f3e700cc3fa
J Luetkens <megahoschi@googlemail.com>
2026-09-17 23:05:37 +0200

    Add Subway Timing session ID

--- src/game/session.ts
+++ src/game/session.ts
@@ -1,4 +1,4 @@
-export type DungeonId = 'Asteroids' | 'SchoolPongCourt' | 'RooftopInvaders' | 'VideoStorePlatform';
+export type DungeonId = 'Asteroids' | 'SchoolPongCourt' | 'RooftopInvaders' | 'VideoStorePlatform' | 'SubwayTiming';
 
 export type Session = {
     clearedDungeonIds: DungeonId[];
