3805144524401bf3fb2f25be765fd901dc5ef03c
J Luetkens <megahoschi@googlemail.com>
2026-09-18 23:40:02 +0200

    Register Warehouse Blockworks session ID

--- src/game/session.ts
+++ src/game/session.ts
@@ -1,4 +1,4 @@
-export type DungeonId = 'Asteroids' | 'SchoolPongCourt' | 'RooftopInvaders' | 'VideoStorePlatform' | 'SubwayTiming' | 'LaundromatLabyrinth';
+export type DungeonId = 'Asteroids' | 'SchoolPongCourt' | 'RooftopInvaders' | 'VideoStorePlatform' | 'SubwayTiming' | 'LaundromatLabyrinth' | 'WarehouseBlockworks';
 
 export type Session = {
     clearedDungeonIds: DungeonId[];
